cb859fb09ffb65a21caf1c0df57dd4a0f239f0f3
Priyansh <79632896+Priyansh521@users.noreply.github.com>
2023-09-24 16:42:33 +0530

    Create duplicates from sorted array.java

---
+++ duplicates from sorted array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int x = 1;
+        for(int i =1; i<nums.length; i++){
+            if(nums[i] != nums[i-1]){
+                 nums[x] = nums[i];
+                 x++;
+            }
+        }
+        return x;
+    }
+
+}
