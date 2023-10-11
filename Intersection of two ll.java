8c9661a1b1650324986723c0fdd1d3ef2f7ff3c1
Priyansh <79632896+Priyansh521@users.noreply.github.com>
2023-10-11 21:47:41 +0530

    Create Intersection of two ll.java

---
+++ Intersection of two ll.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode x = headA;
+        ListNode y = headB;
+        while(x!=y){
+            x = x == null? headB : x.next; 
+            y = y == null? headA : y.next; 
+        }
+        return x;
+    }
+}
