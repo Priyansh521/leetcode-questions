d89dd593705e2a9b3a016f21a0ab325a662e4342
Priyansh <79632896+Priyansh521@users.noreply.github.com>
2023-10-30 16:26:35 +0530

    Create remove outermost parenthesis.java

---
+++ remove outermost parenthesis.java
@@ -0,0 +1,20 @@
+lass Solution {
+    public String removeOuterParentheses(String s) {
+        Stack<Character> stack = new Stack<>();
+        StringBuilder str = new StringBuilder();
+        for(int i =0; i<s.length(); i++){
+            if(s.charAt(i) == '('){
+                if(stack.size()>0){
+                    str.append(s.charAt(i));
+                }
+                stack.push(s.charAt(i));
+            }else{
+                stack.pop();
+                if(stack.size()>0){
+                    str.append(s.charAt(i));
+                }
+            }
+        }
+        return str.toString();
+    }
+}
